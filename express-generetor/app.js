de12d330f3b239c5cc947b64a78af3c76ad2a1de
daviiisousa <sousadavi248@gmail.com>
2024-11-04 19:59:53 -0300

    gerando token e senhz com hash

--- express-generetor/app.js
+++ express-generetor/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var usersRouter = require('./routes/users');
+// var adressRouter = require('./routes/adress')
 
 var app = express();
 
@@ -18,7 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/users', usersRouter);
+// app.use('/adress', adressRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express-generetor/controllers/UserController.js
+++ express-generetor/controllers/UserController.js
@@ -1,4 +1,8 @@
 const { User } = require('../models');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+require("dotenv").config()
+
 
 const getAllUsers = async (_req, res) => {
     const users = await User.findAll();
@@ -15,14 +19,49 @@ const createUser = async (req, res) => {
         return res.status(400).json({ error: "Nome, email e senha são obrigatórios." })
     }
     try {
-        const newUser = await User.create({ name, email, password });
-        res.status(201).json(newUser);
+
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ error: "e-mail já cadastrado" })
+        }
+
+        const hasedPassowrd = await bcrypt.hash(password, 10)
+
+        await User.create({ name, email, password: hasedPassowrd });
+        res.status(201).json({ message: `Seja bem vindo ${name}` });
+
     } catch (error) {
         res.status(500).json({ error: "Erro ao criar usuário" })
     }
 }
 
+const loginUser = async (req, res) => {
+    const {email, password} = req.body
+    if(!email || !password) {
+        return res.status(400).json({error: 'email e senha são obrigatorios'})
+    }
+
+    try {
+        const user = await User.findOne({where: {email}});
+        if(!user) {
+            return res.status(404).json({error: 'usuario nao encontrado'})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.status(401).json({error: 'senha invalida'})
+        }
+
+        const token = jwt.sign({id: user.id, email: user.email}, process.env.SECRET, {expiresIn: '1h'} ) 
+        res.status(200).json({token})
+
+    } catch (error) {
+        res.status(500).json({error: 'erro no servidor'});
+    }
+}
+
 module.exports = {
     getAllUsers,
-    createUser
+    createUser,
+    loginUser
 }
