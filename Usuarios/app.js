866904ec1ec9265476ca6f7ad0a84d1c4ebb7365
daviiisousa <sousadavi248@gmail.com>
2024-12-13 14:01:06 -0300

    cors

--- Usuarios/app.js
+++ Usuarios/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 app.use(express.json());
 
+// Configuração do CORS
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://localhost:5173"], 
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+};
+
+app.use(cors(corsOptions));
+
+// Rotas
 const usuarioRoutes = require("./src/routes/usuariosRoutes");
 const enderecosRoutes = require("./src/routes/enderecoRouter");
 
-// Rotas
 app.use("/usuarios", usuarioRoutes);
 app.use("/enderecos", enderecosRoutes);
 
